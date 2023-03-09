d823ec36582e965f551016b64806bfb2e2c6191d
anikinkirill <anikinkirill@yandex-team.ru>
2023-03-09 11:52:49 +0300

    Add necessary imports and update index.html for jsApp

--- shared/src/commonMain/kotlin/com/example/demoapppodlodka/App.kt
+++ shared/src/commonMain/kotlin/com/example/demoapppodlodka/App.kt
@@ -1,13 +1,17 @@
 package com.example.demoapppodlodka
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.material.*
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import kotlinx.coroutines.launch
 
 @Composable
 internal fun App() {
